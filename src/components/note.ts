6c616b5c0ec1cc60fa037ff81fdd90af88032061
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-25 07:35:50 +0300

    feat: add note.ts

---
+++ src/components/note.ts
@@ -0,0 +1,31 @@
+type BackgroundColor = 'red' | 'blue' | 'green' | 'yellow' | 'brown'
+
+type Note = {
+  title: string;
+  body: string;
+  createDate: Date;
+  targetDate: Date;
+  color: BackgroundColor
+};
+
+
+function createNote(deetz: Note): string {
+    const title: string = deetz.title
+    const body: string = deetz.body
+    const tDate: Date = deetz.targetDate
+    const cDate: Date = deetz.createDate
+    const bg: BackgroundColor = deetz.color
+
+  const note: string = `
+    <div id="note" style="color: ${bg};">
+        <h2 id="note-title">${title}</h2>
+        <p id="note-body">${body}</p>
+        <p id="creation-date">${cDate}</p>
+        <p id="target-date">${tDate}</p>
+    </div>
+`;
+
+  return note;
+}
+
+export default createNote
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import './style.css'
-import mainPage from './components/page'
+import "./style.css";
+import mainPage from "./components/page";
+import createNote from "./components/note";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   ${mainPage}
-`
+`;
+
+  // ${createNote ({
+  //   title: 'Note Title',
+  //   body: 'Note Body',
+  //   createDate: new Date,
+  //   targetDate: new Date,
+  //   color: 'red'
+  // })}
