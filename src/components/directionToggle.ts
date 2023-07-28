950c934c22b5223c6cff55f6b6cd4a2bf43e007c
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-28 11:36:23 +0300

    refactor: move sortOption and sortSelect to labelAndInput, add directionToggle

---
+++ src/components/directionToggle.ts
@@ -0,0 +1,8 @@
+function directionToggle(): HTMLInputElement {
+    const toggle: HTMLInputElement = document.createElement('input')
+    toggle.type = 'checkbox'
+    return toggle
+}
+
+
+export default directionToggle
--- src/components/labelAndInput.ts
+++ src/components/labelAndInput.ts
@@ -1,5 +1,6 @@
-import { noteColors } from "../utils/util";
+import { noteColors, sortMethods } from "../utils/util";
 import { colorObject } from "../utils/types";
+import directionToggle from "./directionToggle";
 
 function createLabel(value: string, classes: string[]): HTMLLabelElement {
   const l = document.createElement("label");
@@ -48,4 +49,30 @@ const colorSelect = (): HTMLSelectElement => {
   return colorSelect;
 };
 
-export { createInput, createLabel, colorSelect };
+function sortOption (option: string): HTMLOptionElement {
+  const o: HTMLOptionElement = document.createElement("option");
+  o.innerText = option;
+  o.value = option;
+  return o;
+};
+
+function sortSelect (): HTMLDivElement {
+  const selectContainer: HTMLDivElement = document.createElement('div')  
+  const sortSelect: HTMLSelectElement = document.createElement("select");
+  sortSelect.id = "color-select";
+  sortSelect.required = true;
+  sortSelect.ariaLabel = "Default select element";
+
+  sortMethods.forEach((method) => {
+    sortSelect.appendChild(sortOption(method));
+  });
+
+  const sortToggle: HTMLInputElement = directionToggle()
+
+  selectContainer.appendChild(sortSelect)
+  selectContainer.appendChild(sortToggle)
+  return selectContainer;
+};
+
+
+export { createInput, createLabel, colorSelect, sortSelect };
