78fbc8763c775836d3e7a4a287049341f6342231
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-28 12:40:01 +0300

    dev: add scaffold for funcitions sortBy and sortNotes

--- src/utils/sort.ts
+++ src/utils/sort.ts
@@ -1,10 +1,22 @@
 import { Note } from "./types"
 import { sortMethods } from "./util"
 
-function sortBy (collection: Note[]) {
+function sortBy (method: string): () => string {
+    // receive sortMethod: string
+    // return sortFunction for appropriate method
+    return () => ''
+}
 
+function sortNotes (notes: Note[], method: () => []): Note[] {
+    // receive note array and sort method
+    // use Array.sort to sort notes by method
+    // return sorted array
+
+    const sortedNotes: Note[] = notes
+    return sortedNotes
 }
 
 export {
-    sortBy
+    sortBy,
+    sortNotes
 }
