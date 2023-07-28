45ca19ffa9dc37c2a3060e790a5b2ae925be9258
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-28 10:15:49 +0300

    refactor: seperate create input functions from createNoteForm

--- src/components/createNoteForm.ts
+++ src/components/createNoteForm.ts
@@ -1,6 +1,7 @@
 import newNote from "./createNote";
+import { colorSelect, createInput, createLabel } from "./labelAndInput";
 import { BackgroundColor } from "../utils/types";
-import { noteColors, formatDate } from "../utils/util";
+import { formatDate } from "../utils/util";
 
 function makeNoteForm() {
   const date = new Date();
@@ -92,52 +93,5 @@ function makeNoteForm() {
 
   return formContainer;
 }
-
-function createLabel(value: string, classes: string[]) {
-  const l = document.createElement("label");
-  DOMTokenList.prototype.add.apply(l.classList, classes);
-  l.innerText = value;
-  return l;
-}
-
-function createInput(
-  type: string,
-  id: string,
-  classes: string[],
-  required: boolean,
-  other?: string[][]
-) {
-  const inp = document.createElement("input");
-  inp.type = type;
-  inp.id = id;
-  DOMTokenList.prototype.add.apply(inp.classList, classes);
-  inp.required = required;
-  if (other) {
-    other.forEach((attr) => {
-      inp.setAttribute(attr[0], attr[1]);
-    });
-  }
-  return inp;
-}
-
-const colorOption = (color: BackgroundColor) => {
-  const o = document.createElement("option");
-  o.innerText = color;
-  o.value = color;
-  return o;
-};
-
-const colorSelect = () => {
-  const colorSelect = document.createElement("select");
-  colorSelect.id = "color-select";
-  colorSelect.required = true;
-  colorSelect.ariaLabel = "Default select element";
-
-  noteColors.forEach(color => {
-    colorSelect.appendChild(colorOption(color))
-  })
-
-  return colorSelect;
-};
 
 export default makeNoteForm;
