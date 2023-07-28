085738148dd431d183ec89adc88befa48d010b59
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-28 12:22:20 +0300

    add direction button styles and logic

--- src/components/directionToggle.ts
+++ src/components/directionToggle.ts
@@ -1,8 +1,20 @@
-function directionToggle(): HTMLInputElement {
-    const toggle: HTMLInputElement = document.createElement('input')
-    toggle.type = 'checkbox'
+function directionToggle(): HTMLDivElement {
+    const toggle = document.createElement('div')
+    toggle.id = 'box'
+    toggle.addEventListener('click', () => {
+        toggle.classList.toggle('up')
+        if (toggle.classList.contains('up')) {
+            console.log('check')
+        } else {
+            console.log('uncheck')
+        }
+    })
+
+    const arrow = document.createElement('div')
+    arrow.id = 'arrow'
+    
+    toggle.appendChild(arrow)
     return toggle
 }
-
 
 export default directionToggle
--- src/components/labelAndInput.ts
+++ src/components/labelAndInput.ts
@@ -58,6 +58,7 @@ function sortOption (option: string): HTMLOptionElement {
 
 function sortSelect (): HTMLDivElement {
   const selectContainer: HTMLDivElement = document.createElement('div')  
+  selectContainer.classList.add('d-flex', 'align-items-center', 'justify-content-between', 'w-25')
   const sortSelect: HTMLSelectElement = document.createElement("select");
   sortSelect.id = "color-select";
   sortSelect.required = true;
@@ -67,7 +68,7 @@ function sortSelect (): HTMLDivElement {
     sortSelect.appendChild(sortOption(method));
   });
 
-  const sortToggle: HTMLInputElement = directionToggle()
+  const sortToggle: HTMLDivElement = directionToggle()
 
   selectContainer.appendChild(sortSelect)
   selectContainer.appendChild(sortToggle)
