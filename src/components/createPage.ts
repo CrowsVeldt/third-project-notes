aef6e860bdd1d30f4e20f958248bf513a9571723
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-29 09:27:19 +0300

    refactor: makeMain into 3 seperate function, fix footer position

--- src/components/createPage.ts
+++ src/components/createPage.ts
@@ -1,12 +1,9 @@
-import makeSearchBar from "./createSearchBar";
+// import makeSearchBar from "./createSearchBar";
+// import makeWipeButton from "./deleteAllButton";
+// import sortSelect from "../form components/sortSelector";
 import { makeNoteContainer } from "./noteContainer";
-import makeWipeButton from "./deleteAllButton";
-import sortSelect from "../form components/sortSelector";
 
-function makeMain(): HTMLDivElement {
-  const mainContainer: HTMLDivElement = document.createElement("div");
-  mainContainer.id = "main-page";
-
+function makeHeader(): HTMLHeadElement {
   const head: HTMLHeadElement = document.createElement("header");
   head.classList.add("container-fluid", "text-center", "border");
   const title: HTMLHeadingElement = document.createElement("h1");
@@ -15,29 +12,37 @@ function makeMain(): HTMLDivElement {
   head.appendChild(title);
 
   // VVVVVV - in header for ease of testing - TODO move
-  head.appendChild(makeWipeButton());
-  head.appendChild(makeSearchBar());
-  head.appendChild(sortSelect())
+  // head.appendChild(makeWipeButton());
+  // head.appendChild(makeSearchBar());
+  // head.appendChild(sortSelect())
   // ^^^^^^ --------------------------------
 
-  const noteContainer: HTMLDivElement = makeNoteContainer();
+  return head
+}
 
+function makeMain(): HTMLDivElement {
+  const mainContainer: HTMLDivElement = document.createElement("div");
+  mainContainer.id = "main-page";
+  mainContainer.classList.add('flex-grow-1')
+
+  const noteContainer: HTMLDivElement = makeNoteContainer();
+  mainContainer.appendChild(noteContainer);
+
+  return mainContainer;
+}
+
+function makeFooter (): HTMLElement {
   const foot: HTMLElement = document.createElement("footer");
   foot.classList.add(
     "container-fluid",
     "border",
-    "text-center",
-    "bottom-0"
+    "text-center"
   );
   const footTitle: HTMLHeadingElement = document.createElement("h2");
   footTitle.innerText = "Footer";
   foot.appendChild(footTitle);
 
-  mainContainer.appendChild(head);
-  mainContainer.appendChild(noteContainer);
-  mainContainer.appendChild(foot);
-
-  return mainContainer;
+  return foot
 }
 
-export default makeMain;
+export { makeFooter, makeHeader, makeMain }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
 import "./style.css";
-import mainPage from "./components/createPage";
+import {makeFooter, makeHeader, makeMain } from "./components/createPage";
 
-document.querySelector<HTMLDivElement>("#app")!.appendChild(mainPage())
+const app = document.querySelector<HTMLDivElement>("#app")!
+app.style.height = '100vh'
+
+const container = document.createElement('div')
+container.classList.add('d-flex', 'flex-column', 'h-100')
+
+container.appendChild(makeHeader())
+container.appendChild(makeMain())
+container.appendChild(makeFooter())
+
+app.appendChild(container)
