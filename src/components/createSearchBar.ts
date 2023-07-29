9e9f232b95829edb82ac507f19c31f9cc7e8118b
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-29 13:55:07 +0300

    refactor: use createInput in createSearchBar

--- src/components/createSearchBar.ts
+++ src/components/createSearchBar.ts
@@ -1,20 +1,19 @@
-import { createLabel } from "../form components/labelAndInput";
+import { createInput, createLabel } from "../form components/labelAndInput";
 import { resetNoteContainer } from "./noteContainer";
 import { searchNotes } from "../utils/storage";
 
 function makeSearchBar(): HTMLDivElement {
   const barContainer: HTMLDivElement = document.createElement("div");
-  const barLabel: HTMLLabelElement = createLabel("Search", []);
 
-  const bar: HTMLInputElement = document.createElement("input");
-  bar.type = "text";
-  bar.addEventListener("input", (event: Event) => {
-    const barInput: HTMLInputElement = event.target as HTMLInputElement;
+  const searchBar: HTMLInputElement = createInput("text", "search-bar", [], false, [
+    ["placeholder", "Search"],
+  ]);
+  searchBar.addEventListener("input", (event: Event) => {
+    const barInput = event.target as HTMLInputElement;
     resetNoteContainer(searchNotes(barInput.value)!);
   });
 
-  barContainer.appendChild(barLabel);
-  barContainer.appendChild(bar);
+  barContainer.appendChild(searchBar);
 
   return barContainer;
 }
