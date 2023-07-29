383cc05a0f16ac6508047195e26185a9d6455903
CrowsVeldt <zechy.mahler@gmail.com>
2023-07-29 19:09:50 +0300

    ui: reposition and resize add note button

--- src/components/createAddNoteButton.ts
+++ src/components/createAddNoteButton.ts
@@ -4,10 +4,16 @@ function addNoteButton(): HTMLButtonElement {
   btn.style.width = "50px";
   btn.style.height = "50px";
   btn.id = `toggle-add-note`;
+  btn.classList.add(
+    "position-fixed", 
+    "end-0", 
+    "bottom-0",
+    'fs-1'
+    );
 
   btn.addEventListener("click", () => {
-    const form = document.getElementById('note-form')!
-    form.classList.toggle('d-flex')
+    const form = document.getElementById("note-form")!;
+    form.classList.toggle("d-flex");
   });
 
   return btn;
