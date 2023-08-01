0b1107358cfb38047a9fa7dfce8e9cb3c26294dc
CrowsVeldt <zechy.mahler@gmail.com>
2023-08-01 19:02:05 +0300

    dev: add alternate create form file

---
+++ src/components/createForm.ts
@@ -0,0 +1,22 @@
+import newElement from "../utils/newElement";
+import { elementParams } from "../utils/types";
+
+function createForm() {
+    // form heading
+    // title input
+    // body input
+    // target date input
+    // color input
+    // action button
+}
+
+const formHeading = newElement({
+    type: 'h3',
+    class: ['form-label'],
+    content: 'Title'
+})
+
+// const titleLabel 
+// const titleInput
+
+
