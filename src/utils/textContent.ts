3b4d694031588f19c771694e825915cf5dc71d72
CrowsVeldt <zechy.mahler@gmail.com>
2023-08-13 13:59:28 +0300

    dev: add hebrew text to textContent

--- src/utils/textContent.ts
+++ src/utils/textContent.ts
@@ -65,6 +65,72 @@ const langOptions = {
       updateNote: "Update note details? This cannot be undone",
     },
   },
+  hebrew: {
+    colors: [
+      { name: "ללא צבע", value: "none" },
+      { name: "אדום", value: "salmon" },
+      { name: "כחול", value: "lightblue" },
+      { name: "ירוק", value: "lightgreen" },
+      { name: "צהוב", value: "gold" },
+      { name: "חום", value: "tan" },
+    ],
+    sortMethods: [
+      { method: "תאריך ייצור" },
+      { method: "כותרת" },
+      { method: "תאריך יעד" },
+      { method: "צבע" },
+    ],
+    tooltips: {
+      plusButton: "יצור פתק חדש",
+      xButton: "סגור חלון",
+      settings: "פתח/סגור הגדרות",
+    },
+    placeholders: {
+      searchBar: "חיפוש",
+    },
+    elementText: {
+      wipeButton: "למחוק הכל",
+      fullNote: {
+        dates: {
+          createDate: "תאריך ייצור: ",
+          targetDate: "תאריך יעד: ",
+        },
+      },
+      note: {
+        dates: {
+          createDate: "תאריך יעד - ",
+          targetDate: "תאריך יצור - ",
+        },
+      },
+      noteForm: {
+        formTitle: {
+          add: "פתק חדש",
+          update: "עריכה",
+        },
+        noteTitle: "כותרת",
+        body: "תוכן",
+        date: "יעד",
+        color: "צבע",
+        actionButton: {
+          add: "להוסיף פתק",
+          update: "לעדכן פתק",
+        },
+      },
+      pageElements: {
+        pageTitle: "פתקים",
+        footer: "פוטר",
+      },
+      settings: {
+        title: "הגדרות",
+        langToggle: "עברית",
+      },
+    },
+    confirmations: {
+      wipeButton: "למחוק את כל הפתקים? אי אפשר לשחזר",
+      deleteNote: "למחוק?",
+      updateNote: "לעדכן?",
+    },
+  },
 };
 
 export default langOptions;
